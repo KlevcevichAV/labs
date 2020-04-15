82a6aef3b3a2031e9c9ce36971a8348203108ea8
sanchir <alexandr.klevcevich@gmail.com>
2020-04-15 22:41:34 +0300

    add project

---
+++ src/sample/Sportsman.java
@@ -0,0 +1,84 @@
+package sample;
+
+public class Sportsman {
+    private String fullName;
+    private String structure;
+    private String position;
+    private int title;
+    private String kindOfSport;
+    private String category;
+
+    void setStructure(int structure){
+        switch (structure){
+            case 0:{
+                this.structure = "основной";
+                break;
+            }
+            case 1:{
+                this.structure = "запасной";
+                break;
+            }
+            default:{
+                this.structure = "n/a";
+            }
+        }
+    }
+
+    void setCategory(int category){
+        switch (category){
+            case 0:{
+                this.category = "мастер спорта";
+                break;
+            }
+            case 1:{
+                this.category = "кмс";
+                break;
+            }
+            case 2:{
+                this.category = "3-й разряд";
+                break;
+            }
+            case 3:{
+                this.category = "2-й разряд";
+                break;
+            }
+            default:{
+                this.category = "1-й юношеский";
+            }
+        }
+    }
+
+    Sportsman(String fullName, int structure, String position, int title, String kindOfSport, int category){
+        this.fullName = fullName;
+        setStructure(structure);
+        this.position = position;
+        this.title = title;
+        this.kindOfSport = kindOfSport;
+        setCategory(category);
+
+    }
+
+    public int getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getKindOfSport() {
+        return kindOfSport;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getStructure() {
+        return structure;
+    }
+}
