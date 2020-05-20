5aff866bdc19ea4f51aa5b626bb8251d2b34fb73
sanchir <alexandr.klevcevich@gmail.com>
2020-05-20 03:28:51 +0300

    output are added

--- MRZvIS/MRZvIS_2/main.cpp
+++ MRZvIS/MRZvIS_2/main.cpp
@@ -21,6 +21,26 @@ vector<double> inputMatrix(int n) {
     return result;
 }
 
+void outputMatrix(vector<double> matrix){
+    for(int i = 0; i < matrix.size(); i++){
+        cout << matrix[i] << " ";
+    }
+    cout << "\n";
+}
+
+void outputMatrix(vector<vector<double>> matrix){
+    for(int i = 0; i < matrix.size(); i++){
+        outputMatrix(matrix[i]);
+    }
+    cout << "\n";
+}
+
+void outputMatrix(vector<vector<vector<double>>> matrix){
+    for(int i = 0; i < matrix.size(); i++){
+        outputMatrix(matrix[i]);
+    }
+}
+
 vector<vector<double>> inputMatrix(int n, int m) {
     vector<vector<double>> result;
     for (int i = 0; i < n; i++) {
@@ -67,18 +87,21 @@ void outputSizeMatrix(int P, int M, int Q) {
     cout << P << " " << M << " " << Q << endl;
 }
 
-
-
 int main() {
     int P, M, Q;
     inputSizeMatrix(P, M, Q);
-    vector<vector<double>> matrixA = inputMatrix(P, M);
-    vector<vector<double>> matrixB = inputMatrix(M, Q);
-    vector<double> matrixE = inputMatrix(M);
-    vector<vector<double>> matrixG = inputMatrix(P, Q);
+    vector<vector<double>> matrixA = initialize(P, M);
+    vector<vector<double>> matrixB = initialize(M, Q);
+    vector<double> matrixE = initialize(M);
+    vector<vector<double>> matrixG = initialize(P, Q);
     vector<vector<vector<double>>> matrixD = initialize(P,M,Q);
     vector<vector<vector<double>>> matrixF = initialize(P,M,Q);
     vector<vector<double>> matrixC = initialize(P, Q);
     std::cout << "Hello, World!" << std::endl;
+    outputMatrix(matrixE);
+    cout << "\n";
+    outputMatrix(matrixA);
+    cout << "\n";
+    outputMatrix(matrixD);
     return 0;
 }
